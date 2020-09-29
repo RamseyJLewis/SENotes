9595fd92621ff70316ff0b244476dee02fb4c7f2
Ramsey lewis <ramseylewis99@gmail.com>
2020-09-29 16:02:05 -0400

    notes

---
+++ test.js
@@ -0,0 +1,17 @@
+
+function basicOp( value1, value2)
+{ 
+    return value1 + value2;
+}
+console.log(basicOp( 4, 7))
+
+
+var summation = function (num) {
+    var count = 0;
+    for(var i=0, n=array.length; i < n; i++) 
+    { 
+       count += array[i]; 
+    }
+
+}
+console.log(2,(1))
